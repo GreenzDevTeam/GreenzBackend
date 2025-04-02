2aad43107533e8d752cc6e680273e121087585fb
GreenzDevTeam <greenz.devteam@gmail.com>
2025-04-02 19:43:39 +0530

    INIT

---
+++ src/main/java/com/greenz/greenzbackend/GreenzBackendApplication.java
@@ -0,0 +1,13 @@
+package com.greenz.greenzbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenzBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenzBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/greenz/greenzbackend/GreenzBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greenz.greenzbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenzBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
